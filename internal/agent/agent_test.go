1f437c2ca89b4c886b798463fef11a3a6f02d06e
hwipl <33433250+hwipl@users.noreply.github.com>
2022-07-30 19:48:23 +0200

    Add tests for agent
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/agent/agent_test.go
@@ -0,0 +1,31 @@
+package agent
+
+import (
+	"testing"
+
+	"github.com/T-Systems-MMS/fw-id-agent/internal/config"
+)
+
+// TestAgentStartStop tests Start and Stop of Agent
+func TestAgentStartStop(t *testing.T) {
+	config := &config.Config{}
+	agent := NewAgent(config)
+	agent.Start()
+	agent.Stop()
+}
+
+// TestNewAgent tests NewAgent
+func TestNewAgent(t *testing.T) {
+	config := &config.Config{}
+	agent := NewAgent(config)
+	if agent == nil ||
+		agent.tnd == nil ||
+		agent.done == nil ||
+		agent.closed == nil {
+
+		t.Errorf("got nil, want != nil")
+	}
+	if config != agent.config {
+		t.Errorf("got %p, want %p", agent.config, config)
+	}
+}
