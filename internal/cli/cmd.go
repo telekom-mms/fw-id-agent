188ed2c396ed7d258f569c45ec3603f1c79dec8a
hwipl <33433250+hwipl@users.noreply.github.com>
2023-04-11 11:43:58 +0200

    Add command "status" to CLI
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cli/cmd.go
+++ internal/cli/cmd.go
@@ -1,13 +1,15 @@
 package cli
 
 import (
+	"flag"
+
 	"github.com/T-Systems-MMS/fw-id-agent/internal/api"
 	"github.com/T-Systems-MMS/fw-id-agent/internal/status"
 	log "github.com/sirupsen/logrus"
 )
 
-// Run is the main entry point
-func Run() {
+// getStatus retrieves the agent status and prints it
+func getStatus() {
 	client := api.NewClient(api.GetUserSocketFile())
 	b := client.Query()
 	if b == nil {
@@ -21,4 +23,14 @@ func Run() {
 	log.Printf("Trusted Network: %t", status.TrustedNetwork)
 	log.Printf("Logged In: %t", status.LoggedIn)
 	log.Printf("Config: %#v", *status.Config)
+}
+
+// Run is the main entry point
+func Run() {
+	flag.Parse()
+	command := flag.Arg(0)
+	switch command {
+	case "status":
+		getStatus()
+	}
 }
