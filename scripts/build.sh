0786fd09b670ef12f7452c7d3e20efa460e62565
hwipl <33433250+hwipl@users.noreply.github.com>
2023-05-06 10:02:55 +0200

    Add CLI to build script
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,9 @@ GIT_TAG=$(git describe --tags --abbrev=0)
 GIT_COMMIT=$(git rev-list -1 HEAD)
 GIT_VERSION="$GIT_TAG-$GIT_COMMIT"
 
-VERSION="github.com/T-Systems-MMS/fw-id-agent/internal/agent.version=$GIT_VERSION"
+GO_MODULE="github.com/T-Systems-MMS/fw-id-agent"
+AGENT_VERSION="$GO_MODULE/internal/agent.version=$GIT_VERSION"
+CLI_VERSION="$GO_MODULE/internal/cli.version=$GIT_VERSION"
 
-go build -ldflags "-X $VERSION" ./cmd/fw-id-agent
+go build -ldflags "-X $AGENT_VERSION" ./cmd/fw-id-agent
+go build -ldflags "-X $CLI_VERSION" ./cmd/fw-id-cli
