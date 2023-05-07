8204e7f6c9ec7b3425111e13042abe17f219c1d1
hwipl <33433250+hwipl@users.noreply.github.com>
2023-05-07 10:10:57 +0200

    Use Agent version in CLI
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,7 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 GIT_VERSION="$GIT_TAG-$GIT_COMMIT"
 
 GO_MODULE="github.com/T-Systems-MMS/fw-id-agent"
-AGENT_VERSION="$GO_MODULE/internal/agent.version=$GIT_VERSION"
-CLI_VERSION="$GO_MODULE/internal/cli.version=$GIT_VERSION"
+VERSION="$GO_MODULE/internal/agent.Version=$GIT_VERSION"
 
-go build -ldflags "-X $AGENT_VERSION" ./cmd/fw-id-agent
-go build -ldflags "-X $CLI_VERSION" ./cmd/fw-id-cli
+go build -ldflags "-X $VERSION" ./cmd/fw-id-agent
+go build -ldflags "-X $VERSION" ./cmd/fw-id-cli
