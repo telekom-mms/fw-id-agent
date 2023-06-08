a82bc2976e8d70dbe0949fb5b713f937cbec4255
Stefan Schubert <22217677+malaupa@users.noreply.github.com>
2023-06-08 17:13:26 +0200

    use no flag instead of --user
    * reason: the script runs not with user env
      and --global seems to throw an error in future
    * add dependency on helper scripts

--- build/package/postinstall.sh
+++ build/package/postinstall.sh
@@ -1,24 +1,24 @@
 #!/bin/sh -e
-# taken from https://github.com/Debian/debhelper/blob/master/dh
+# taken from https://git.launchpad.net/ubuntu/+source/debhelper/tree/autoscripts/postinst-systemd-user-enable?h=applied/13.6ubuntu1
 
 UNIT='fw-id-agent.service'
 
 case "$1" in
-  'configure' |  'abort-upgrade' | 'abort-deconfigure' | 'abort-remove')
-    # systemctl daemon-reload
-    # systemctl --global enable $UNIT
-    # This will only remove masks created by d-s-h on package removal.
-    deb-systemd-helper --user unmask $UNIT >/dev/null || true
+  'configure' | 'abort-upgrade' | 'abort-deconfigure' | 'abort-remove')
+    if [ -z "${DPKG_ROOT:-}" ] ; then
+      # This will only remove masks created by d-s-h on package removal.
+      deb-systemd-helper --user unmask $UNIT >/dev/null || true
 
-    # was-enabled defaults to true, so new installations run enable.
-    if deb-systemd-helper --quiet --user was-enabled $UNIT ; then
-      # Enables the unit on first installation, creates new
-      # symlinks on upgrades if the unit file has changed.
-      deb-systemd-helper --user enable $UNIT >/dev/null || true
-    else
-      # Update the statefile to add new symlinks (if any), which need to be
-      # cleaned up on purge. Also remove old symlinks.
-      deb-systemd-helper --user update-state $UNIT >/dev/null || true
+      # was-enabled defaults to true, so new installations run enable.
+      if deb-systemd-helper --quiet --user was-enabled $UNIT ; then
+        # Enables the unit on first installation, creates new
+        # symlinks on upgrades if the unit file has changed.
+        deb-systemd-helper --user enable $UNIT >/dev/null || true
+      else
+        # Update the statefile to add new symlinks (if any), which need to be
+        # cleaned up on purge. Also remove old symlinks.
+        deb-systemd-helper --user update-state $UNIT >/dev/null || true
+      fi
     fi
     ;;
 esac
